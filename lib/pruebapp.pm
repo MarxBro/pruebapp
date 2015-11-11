0e5ecbbe90fee1a7529abde8de11c60fecfb811b
MarxBro <allthemarxbrothers@gmail.com>
2015-11-11 17:39:32 -0300

    Funciones de edición y borrado de entradas, silly backend y admin pass input

--- lib/pruebapp.pm
+++ lib/pruebapp.pm
@@ -8,19 +8,14 @@ our $VERSION = '0.1';
 
 get '/' => sub {
     my $data_file = config->{pruebapp}{JSON};
+    my $autor = config->{pruebapp}{autor};
+    my $descripcion = config->{pruebapp}{descripcion};
     my $json = -e $data_file ? read_file $data_file : '{}';
     my $data = from_json $json;
-    template 'index', { data => $data};
+    template 'index', { data => $data, autor => $autor, descripcion => $descripcion};
 };
 
-get '/do' => sub {
-    template 'formulario';
-};
-
-get '/add' => sub {
-    template 'add';
-};
-
+#Mostrar entradas.
 get '/page/*' => sub {
     my ($articulo)  = splat;
     my $data_file   = config->{pruebapp}{JSON};
@@ -32,6 +27,50 @@ get '/page/*' => sub {
     template 'articulo', { titulo => $titulo, contenido => $contenido };
 };
 
+
+# Tareas de administración, editar y borrar
+#       A no olvidarse de cambiar el password bufarra de las configs.
+
+get '/page/*/ed/*' => sub {
+    my ($articulo, $tk) = splat;
+    my $passwd_admin = config->{pruebapp}{admin_p};
+    if ($tk eq $passwd_admin){
+        # editar
+        # abrir ese en el formulario y postinguearlo.
+        my $data_file = config->{pruebapp}{JSON};
+        my $json = -e $data_file ? read_file $data_file : '{}';
+        my $data = from_json $json;
+        my $titulo_editable = $data->{$articulo}{titulo};
+        my $contenido_editable = $data->{$articulo}{contenido};
+        # antes de recargar las nuevas cosas, borrar las viejas.
+        delete $data->{$articulo};
+        write_file $data_file, to_json $data;
+        template '/formulario', { titulo => $titulo_editable, contenido => $contenido_editable};
+    } else {
+        redirect '/';
+    }
+};
+
+get '/page/*/rm/*' => sub {
+    my ($articulo, $tk) = splat;
+    my $passwd_admin = config->{pruebapp}{admin_p};
+    if ($tk eq $passwd_admin){
+        # Borrar
+        my $data_file = config->{pruebapp}{JSON};
+        my $json = -e $data_file ? read_file $data_file : '{}';
+        my $data = from_json $json;
+        delete $data->{$articulo};
+        write_file $data_file, to_json $data;
+        redirect '/';
+    } else {
+        redirect '/';
+    }
+};
+
+#Escribir cosillas.
+get '/do' => sub {
+    template 'formulario';
+};
 post '/do' => sub {
     my $titulo      = params->{'titulo'};
     my $contenido   = params->{'contenido'};
@@ -43,7 +82,7 @@ post '/do' => sub {
         my $json = -e $data_file ? read_file $data_file : '{}';
         my $data = from_json $json;
         my $ya = time;
-        my $date = strftime ("%d/%m/%Y", localtime(time()));
+        my $date = strftime ("%d/%m/%Y - %H:%M:%S", localtime(time()));
         $data->{$ya} = {
             titulo => params->{'titulo'},
             contenido => params->{'contenido'},
